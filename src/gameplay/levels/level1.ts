ed62aeeb86d18b1ca475b8b8f7209fc20d56ae77
alantheprice <alan@theprices.us>
2021-01-12 19:24:20 -0600

    Refactored Waves array

--- src/gameplay/levels/level1.ts
+++ src/gameplay/levels/level1.ts
@@ -5,20 +5,18 @@ import { PieceFactory } from "../../pieces/types";
 import { Level, Wave} from "../types";
 
 export function getLevel1(pieceFactory: PieceFactory, edges: Rect): Level {
-  function wave1() {
-    return [
-      pieceFactory(getSnap(), point(edges.w / 2 - 40, 0)),
-      pieceFactory(getSnap(), point(edges.w / 2, 0)),
-    ];
-  }
 
   function getWave(waveNumber: number): Wave {
     // here we would return either snaps or fighters, and fit the number to the screen width.
     // additionally we could make the health of the fighters increase over the waves to make them more difficult.
     // other ideas....
-    return {
+    
+    return waveNumber % 2 === 0 ? {
       pieces: snaps(), 
       durationInSeconds: 3
+    } : {
+      pieces: fighters(),                         
+      durationInSeconds: 6
     }
   }
 
@@ -32,90 +30,13 @@ export function getLevel1(pieceFactory: PieceFactory, edges: Rect): Level {
 
   function fighters() {
     return [
-      pieceFactory(getFighter(), point(edges.w / 2 - 180, 0)),
-      pieceFactory(getFighter(), point(edges.w / 2 - 240, 0)),
-      pieceFactory(getFighter(), point(edges.w / 2 + 60, 0)),
+      pieceFactory(getFighter(), point(edges.w / 2, 0)),
+      pieceFactory(getFighter(), point(edges.w / 2 - 120, 0)),
+      pieceFactory(getFighter(), point(edges.w / 2 + 120, 0)),
     ];
   }
 
   return {
-    waves: [
-      {
-        pieces: wave1(),
-        durationInSeconds: 5,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 8,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),  
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 3,
-      },
-      {
-        pieces: fighters(),
-        durationInSeconds: 6,
-      },
-      {
-        pieces: snaps(),
-        durationInSeconds: 8,
-      },
-    ],
+    waves: new Array(20).join(",").split(",").map((_, index) => getWave(index))
   };
 }
