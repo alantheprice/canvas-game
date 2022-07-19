6d76f44677fd507962f361b625309a31ece10f74
alan.price <alan@inicion.com>
2022-07-19 09:20:04 -0500

    Trigger build

--- vb-build.sh
+++ vb-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 npm install
 npx webpack --config webpack.config.js --production
 mkdir ./build
-cp -r ./public/ ./build/
 cp sw.js ./build/sw.js
-cp -r ./dist/ ./build/
+cp -r ./public/* ./build/
+cp -r ./dist/* ./build/
