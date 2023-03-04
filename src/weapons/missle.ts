363a42e9fa649bcaf41e2ab849097debca72fbb3
Alan <alanp@localhost.localdomain>
2023-03-04 14:17:39 +0000

    Updated missle look and performance

--- src/weapons/missle.ts
+++ src/weapons/missle.ts
@@ -2,24 +2,73 @@ import { rect } from "../drawing/dimensions";
 import preRender from "../drawing/preRender";
 import { Weapon } from "../pieces/types";
 
-const missleWidth = 10;
+const missleWidth = 14;
 const missleHeight = 30;
 
+const missleColor = "#446";
+const highlightColor = "#555";
+const bodyHighlightColor = "#666";
+
 const layoutData = [
-  [
+  [ // missle body
     "r",
-    { x: missleWidth / 3, y: 0, w: missleWidth / 3, h: missleHeight },
-    "#020299",
+    { x: missleWidth / 3, y: 3, w: missleWidth / 3, h: missleHeight - 6 },
+    missleColor,
   ],
-  [
+  [ // missle front
     "p",
     [
-      [2, missleWidth/2],
+      [4, missleWidth/2],
       [missleWidth/2, 0],
-      [missleWidth - 2, missleWidth/2],
+      [missleWidth - 4, missleWidth/2],
     ],
-    "#020299",
+    missleColor,
     3,
+  ],
+  [// body highlight
+    "p", 
+    [
+      [missleWidth/2 - 1, 4],
+      [missleWidth/2 - 1, missleHeight - 14],
+    ],
+    bodyHighlightColor,
+    1,
+  ],
+  [// tail wing left
+    "p", 
+    [
+      [2, missleHeight - 10],
+      [2, missleHeight],
+    ],
+    missleColor,
+    2,
+  ],
+  [// tail wing right
+    "p", 
+    [
+      [missleWidth - 3, missleHeight - 10],
+      [missleWidth - 3, missleHeight],
+    ],
+    missleColor,
+    2,
+  ],
+  [// tail wing left highlight
+    "p", 
+    [
+      [3, missleHeight - 12],
+      [3, missleHeight - 5],
+    ],
+    highlightColor,
+    2,
+  ],
+  [// tail wing right highlight
+    "p", 
+    [
+      [missleWidth - 5, missleHeight - 12],
+      [missleWidth - 5, missleHeight - 5],
+    ],
+    highlightColor,
+    2,
   ],
 ];
 
@@ -29,7 +78,7 @@ const preRendered = preRender({
 });
 
 export const MISSLE: Weapon = {
-  speed: 50,
+  speed: 450,
   power: 50,
   layout: layoutData,
   preRendered: preRendered,
