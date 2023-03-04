cc3720cb657fdbf274f9f89611d11fe9d7d6cc25
Alan <alanp@localhost.localdomain>
2023-03-04 13:52:18 +0000

    Update to add missles

--- src/weapons/missle.ts
+++ src/weapons/missle.ts
@@ -2,46 +2,41 @@ import { rect } from "../drawing/dimensions";
 import preRender from "../drawing/preRender";
 import { Weapon } from "../pieces/types";
 
-const missleWidth = 12;
+const missleWidth = 10;
 const missleHeight = 30;
 
 const layoutData = [
   [
-    "c",
-    { x: missleWidth / 2, y: missleHeight / 2, diameter: missleWidth / 3.14 },
-    "#944",
-    { width: 4, color: "#844" },
+    "r",
+    { x: missleWidth / 3, y: 0, w: missleWidth / 3, h: missleHeight },
+    "#020299",
   ],
   [
-    "r",
-    { x: missleWidth / 4, y: missleHeight, w: missleWidth / 2, h: missleHeight },
-    "#755",
-    { width: 4, color: "#866" },
+    "p",
+    [
+      [2, missleWidth/2],
+      [missleWidth/2, 0],
+      [missleWidth - 2, missleWidth/2],
+    ],
+    "#020299",
+    3,
   ],
-//   [
-//     "r",
-//     { x: , y: 15, w: 3, h: fighterHeight - 30 },
-//     "#755",
-//     { width: 4, color: "#866" },
-//     "#F00",
-//     3,
-//   ],
 ];
 
-const preRendered = preRender({
+const preRendered = preRender({ 
   layoutData: layoutData,
   frame: rect(0, 0, 20, 30),
 });
 
 export const MISSLE: Weapon = {
-  speed: 900,
+  speed: 50,
   power: 50,
   layout: layoutData,
   preRendered: preRendered,
   hitLayout: (explosionFrames: number) => [
     [
       "c",
-      { x: 0, y: 5, diameter: Math.floor(explosionFrames / 1.2) },
+      { x: 0, y: 5, diameter: Math.floor(explosionFrames * 2) },
       "#FA5",
       {
         width: 1,
